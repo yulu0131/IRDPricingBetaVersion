285309bb70dd8c91fad04f3c2bcea0679f169131
yu.lu <luyudso@gmail.com>
2023-09-16 18:31:12 +0800

    initial commit

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+==================================
+IRDModelling
+==================================
+
+.. warning:: This documentation updates on 12th September2023!
+
+Analysis and Pricing of Interest Rate Options: A Focus on LPR and SHIBOR-linked Caps and Floors
+=========================================================================================================
+
+In the ever-evolving landscape of Chinese financial derivative markets, understanding the intricacies of interest rate options is paramount. This project endeavors to conduct an exhaustive analysis of interest rate models, specifically focusing on the pricing of interest rate options like caps and floors whose underpinnings are tied to benchmarks such as the Loan Prime Rate (LPR) and the Shanghai Interbank Offered Rate (SHIBOR).
+
+To provide a comprehensive view, the models will be delineated into three distinct categories: the Black model, short-term interest rate models, and the Libor Market Model (often referred to as the Brace-Gatarek-Musiela or BGM model). Through this classification and the corresponding emprical analysis, the project aims to deliver a clear understanding of the methodologies and implications of each model in the context of modern interest rate derivatives.
+
+
+.. toctree::
+   :maxdepth: 2
+   :hidden:
+
+   模型 <ird_models/ird_models>
+   实证分析 <comparison/comparison>
+
---
+++ docs/source/conf.py
@@ -0,0 +1,68 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'IRDModelling'
+copyright = '2023, Yu Lu'
+author = 'Yu Lu'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.1a'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.mathjax',
+              'nbsphinx',
+              'myst_parser'
+            ]
+
+# Add any paths that contain templates here, relative to this directory.
+source_syffux = ['.rst', '.md']
+myst_enable_extensions = ["dollarmath", "amsmath"]
+master_doc = 'index'
+templates_path = ['_templates']
+exclude_patterns = []
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+html_static_path = ['_static']
+
+# Napoleon options
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'numpy': ('https://docs.scipy.org/doc/numpy/', None)
+}
+
+# drop module name
+add_module_names = False
